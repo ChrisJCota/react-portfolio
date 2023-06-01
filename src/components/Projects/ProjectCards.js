0548868056b7b935de539bb7ce91054a9f9a674f
ChrisJCota <chrisjocota@gmail.com>
2023-05-31 21:52:54 -0400

    updated projectinfo and projectCard

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -7,14 +7,13 @@ function DeployedCards() {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
-        <Card.Title>{getBsProps.Title}</Card.Title>
+        <Card.Title>{props.title}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {props.description}
         </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Link href={props.gitRepo}>Github Repo</Card.Link>
+        <Card.Link href={props.deployedLink}>Deployed</Card.Link>
       </Card.Body>
     </Card>
 
--- src/components/Projects/ProjectInfo.js
+++ src/components/Projects/ProjectInfo.js
@@ -2,5 +2,43 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import ProjectCard from "./ProjectCards";
+import { Container, Row, Col } from "react-bootstrap";
 
 
+function Projects() {
+    return (
+        <Container className="projects-section">
+            <Container>
+                <h1>
+                    Welcome to my <strong>Projects!</strong>
+                </h1>
+                <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            title="Text-Editor"
+                            description="This project is a text editor which allows the user to create code or notes and retrieve them later on."
+                            gitRepo="https://github.com/ChrisJCota/text-editor"
+                            deployedLink="https://my-text-editor-with-pwa.herokuapp.com/">
+
+                        </ProjectCard>
+                    </Col>
+
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            title="Phudi"
+                            description="This collaborative project is a food reservation app that allows users to login and book a reservation. Then store that reservation on their personal site which shows their previous reservations."
+                            gitRepo="https://github.com/ChrisJCota/phudi"
+                            deployedLink="https://phudireservations.herokuapp.com/">
+
+                        </ProjectCard>
+                    </Col>
+                </Row>
+
+            </Container>
+
+        </Container>
+
+
+
+    );
+}
