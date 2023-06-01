17ef832ba14a0b412440bd2591764ce10f7ab539
Christopher Cota <118009584+ChrisJCota@users.noreply.github.com>
2023-06-01 09:11:25 -0400

    Put in all all projects cards

--- src/components/Projects/ProjectInfo.js
+++ src/components/Projects/ProjectInfo.js
@@ -32,6 +32,44 @@ function Projects() {
 
                         </ProjectCard>
                     </Col>
+
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            title="Social Network API"
+                            description="This is an API where different users can share their thoughts and reactions as well as create a friend list."
+                            gitRepo="https://github.com/ChrisJCota/social-network-api">
+
+                        </ProjectCard>
+                    </Col>
+
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            title="Company Directory"
+                            description="This project lets you open, add, and modify a company directory using a SQL database and node to run the application."
+                            gitRepo="https://github.com/ChrisJCota/company_directory">
+
+                        </ProjectCard>
+                    </Col>
+                    
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            title="JS Quiz"
+                            description="One of my early projects creating a JS quiz using tools from Javascript, utilizing local storage to store data and practicing implementing strategies to create interactive webpages through JS."
+                            gitRepo="https://github.com/ChrisJCota/JS-Quiz"
+                            deployedLink="https://chrisjcota.github.io/JS-Quiz/">
+
+                        </ProjectCard>
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            title="readMe Generator"
+                            description="This project generates professional ReadMe's using inquirer, Javascript and node."
+                            gitRepo="https://github.com/ChrisJCota/ReadMeGenerator">
+                            
+
+                        </ProjectCard>
+                    </Col>
+                    
                 </Row>
 
             </Container>
