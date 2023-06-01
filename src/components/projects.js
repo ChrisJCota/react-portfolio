b870cdc4b4b7adac29b74f070b250b521c91af24
Christopher Cota <118009584+ChrisJCota@users.noreply.github.com>
2023-06-01 10:34:59 -0400

    Update The routes for Bio and Projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,20 @@
 import React, { useState } from 'react';
+import Bio from './components/Projects/Bio';
+import Projects from './components/Projects/ProjectInfo';
 
+function Project({ currentPage }) {
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'about':
+        return <Bio />;
+      case 'contact':
+        return <Projects />;
+      default:
+        return null;
+    }
+  };
 
-export default Projects;
+  return <div>{renderPage()}</div>;
+}
+
+export default Portfolio;
