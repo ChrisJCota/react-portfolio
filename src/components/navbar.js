c4ff5de6ea239bd7cf840d47f80a25743bc8d7f0
Christopher Cota <118009584+ChrisJCota@users.noreply.github.com>
2023-06-01 09:50:13 -0400

    Added links to header href's

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,14 +11,14 @@ function Header() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Projects</Nav.Link>
-                        <Nav.Link href="#github">Github</Nav.Link>
-                        <Nav.Link href="#linkedIn">LinkedIn</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="https://github.com/ChrisJCota">Github</Nav.Link>
+                        <Nav.Link href="https://www.linkedin.com/in/christopherjcota/">LinkedIn</Nav.Link>
                         <Nav.Link href="#resume">Resume</Nav.Link>
 
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
+                        <Nav.Link href="mailto:chrisjocota@gmail.com?subject=Mail From Portfolio">Contact</Nav.Link>
 
                     </Nav>
                 </Navbar.Collapse>
