8033277faf40ddf9a38a257043908c370d89dad2
ChrisJCota <chrisjocota@gmail.com>
2023-06-05 21:49:24 -0400

    resolve bio conlfict

--- src/components/Projects/Bio.js
+++ src/components/Projects/Bio.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 
 import Image from 'react-bootstrap/Image';
-import profilePhoto from '../assets/photos/profilePhoto.jpg'
+//import profilePhoto from '../assets/photos/profilePhoto.jpg'
 
 function Bio() {
   return (
@@ -10,7 +10,7 @@ function Bio() {
       <h2 className="display-3 nav-header">Welcome to my Portfolio!</h2>
       <div className="row">
         <div className="col col-2">
-          <Image src={profilePhoto} className="img-thumbnail" alt="Profile Picture" />
+          <Image src='#' className="img-thumbnail" alt="Profile Picture" />
         </div>
         <p className="bio col-10 fs-6">My name is Chris Cota, and I'm a 26-year-old Web Developer from Boston, MA. <br /><br />I started my web development journey a couple of years back after spending many years as a sales consultant.
           In the last year, I have taken up numerous projects and in December 2022 began a full-stack web development bootcamp at the University of New Hampshire that will be completed in June 2023. <br /><br />
